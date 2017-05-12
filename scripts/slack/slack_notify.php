358a3e7fe6f3f201ea42f3ca9271e2a6450dac47
Matt Cheney <matt@getpantheon.com>
2017-05-12 16:32:55 -0700

    Create and Merge CI Language Updates

--- scripts/slack/slack_notify.php
+++ scripts/slack/slack_notify.php
@@ -19,14 +19,14 @@ switch($slack_type) {
     $slack_agent = 'Pantheon-Merge';
     $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/pantheon.png';
     $slack_color = '#EFD01B';
-    $slack_message = "Merging `". $argv[2] . "` environment into Pantheon `dev` environment...";
+    $slack_message = "Merging Pantheon environment `". $argv[2] . "` Pantheon `dev` environment for GitHub branch `" . $argv[3] . "`.";
     _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
   case 'pantheon_create':
     $slack_agent = 'Pantheon-Create';
     $slack_icon = 'http://live-drupalcon-github-magic.pantheonsite.io/sites/default/files/icons/pantheon.png';
     $slack_color = '#1EFD01B';
-    $slack_message = "Creating Pantheon environment `". $argv[2] . "`...";
+    $slack_message = "Creating Pantheon environment `". $argv[2] . "` for GitHub branch `" . $argv[3] . "`.";
     _slack_tell( $slack_message, $slack_channel, $slack_agent, $slack_icon, $slack_color);
     break;
 }
