3fe700589da2875a4b53afab1d869d0f63b61642
Sean Dietrich <sean.dietrich@inresonance.com>
2017-05-22 22:25:01 -0700

    Converting behat to local database

---
+++ scripts/circleci/settings.local.php
@@ -0,0 +1,14 @@
+<?php
+
+$databases['default']['default'] = array (
+   'database' => 'circle_test',
+   'username' => 'ubuntu',
+   'password' => 'u',
+   'host' => '127.0.0.1',
+   'port' => '3306',
+   'driver' => 'mysql',
+   'prefix' => '',
+   'collation' => 'utf8mb4_general_ci',
+ );
+
+$settings['hash_salt'] = '12345';
