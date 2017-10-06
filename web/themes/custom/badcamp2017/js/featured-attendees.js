00311bc986b01758492004beb9012927ea2c062e
Sean Dietrich <sean.e.dietrich@gmail.com>
2017-10-05 22:47:31 -0700

    Removed swiper module and using owl carousel instead

--- web/themes/custom/badcamp2017/js/featured-attendees.js
+++ web/themes/custom/badcamp2017/js/featured-attendees.js
@@ -1,23 +1,46 @@
 var swiper;
+var owl;
 (function(Drupal, $) {
 
   Drupal.behaviors.featuredAttendees = {
-    attach:function(context) {
-      swiper = new Swiper('.swiper-container', {
-        nextButton: '.swiper-fa-button-next',
-        prevButton: '.swiper-fa-button-prev',
-        slidesPerView: 'auto',
-        paginationClickable: true,
+    attach: function(context) {
+
+      owl = $('.owl-carousel');
+      owl.owlCarousel({
         loop: true,
-        freeMode: true,
-        uniqueNavElements: false,
-        spaceBetween: 30,
-        breakpoints: {
+        margin: 45,
+        nav: false,
+        navText : ["",""],
+        stagePadding: 10,
+        responsive:{
           0: {
-            slidesPerView: 1
+            items: 1
+          },
+          450:{
+            items:2
+          },
+          640: {
+            items: 1
+          },
+          1150:{
+            items: 2
+          },
+          1500:{
+            items: 3
           }
+        },
+        onInitialized: function (event) {
+          $(window).resize();
         }
       });
+
+      $('#feature-attendees-prev').click(function() {
+        owl.trigger('prev.owl.carousel');
+      });
+
+      $('#feature-attendees-next').click(function() {
+        owl.trigger('next.owl.carousel');
+      });
     }
-  }
+  };
 }(Drupal, jQuery));
